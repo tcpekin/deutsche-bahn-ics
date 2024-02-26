0f2d3c7af18ae6268e7d55a34b5b4cad409c52d9
tcpekin <tcpekin@gmail.com>
2024-02-26 23:59:18 +0100

    Update main.js
    add license

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 // @match        https://www.bahn.de/buchung/fahrplan/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bahn.de
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function () {
